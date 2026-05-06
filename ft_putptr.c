4d74739e141ed44b16c77c32412b357af647a38f
remy chavastel <remy_chvst@MacBook-Pro-de-remy.local>
2026-05-06 14:05:43 +0200

    final push

--- ft_putptr.c
+++ ft_putptr.c
@@ -14,21 +14,27 @@
 
 static int	ft_putptr_hex(unsigned long n)
 {
-	int	count;
+	char	*base;
+	int		count;
 
+	base = "0123456789abcdef";
 	count = 0;
 	if (n >= 16)
 		count += ft_putptr_hex(n / 16);
-	count += ft_putchar("0123456789abcdef"[n % 16]);
+	count += ft_putchar(base[n % 16]);
 	return (count);
 }
 
 int	ft_putptr(void *ptr)
 {
-	int	count;
+	unsigned long	address;
+	int				count;
 
+	if (ptr == 0)
+		return (write(1, "0x0", 3));
+	address = (unsigned long)ptr;
 	count = 0;
-	count += ft_putstr("0x");
-	count += ft_putptr_hex((unsigned long)ptr);
+	count += write(1, "0x", 2);
+	count += ft_putptr_hex(address);
 	return (count);
 }
